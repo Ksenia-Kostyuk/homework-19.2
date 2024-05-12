c596d5d2721e11bb6b437556a24015a9ae354532
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-05-12 19:30:21 +0300

    homework 20.1 task 4

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='product', blank=True, null=True, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,)
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    created_at = models.DateField(null=True, verbose_name='Дата создания')
+    updated_at = models.DateField(null=True, verbose_name='Дата последнего изменения')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продкуты'
+
+    def __str__(self):
+        return f'{self.name}, {self.description}, {self.price}'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
@@ -5,6 +7,6 @@ from catalog.views import home, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='index'),
-    path('contacts/', contacts, name='contacts')
-]
+    path("", home, name="index"),
+    path("contacts/", contacts, name="contacts"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
