357f848683e367e4405ed18404322c6b5685847c
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-05-19 16:55:28 +0300

    homework 20.1

--- catalog/management/commands/read_data.py
+++ catalog/management/commands/read_data.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-
+import json
 from catalog.models import Category, Product
 
 
@@ -15,19 +15,24 @@ class Command(BaseCommand):
         product_for_create = []
         category_for_create = []
 
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
         for i in Command.json_read():
-            if i['models'] == 'catalog.category':
+            if i['model'] == 'catalog.category':
                 category_for_create.append(
                     Category(name=i.get('fields').get('name'), pk=i.get('pk'),
                              description=i.get('fields').get('description'))
                 )
-            elif i['models'] == 'catalog.product':
+            elif i['model'] == 'catalog.product':
                 product_for_create.append(
                     Product(pk=i.get('pk'), name=i.get('fields').get('name'),
                             description=i.get('fields').get('description'), image=i.get('fields').get('image'),
-                            category=Category.objects.get(pk=i.get('pk')), price=i.get('fields').get('price'),
+                            category=Category(pk=i.get('pk')), price=i.get('fields').get('price'),
                             created_at=i.get('fields').get('created_at'), updated_at=i.get('fields').get('updated_at')
-                            )
-            Category.objects.bulk_create(category_for_create)
+                            ))
+            else:
+                continue
 
-            Product.objects.bulk_create(product_for_create)
+        Category.objects.bulk_create(category_for_create)
+        Product.objects.bulk_create(product_for_create)
