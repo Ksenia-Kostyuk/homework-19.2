08b762d6c657b68019b55fafbd7da73c2feea25c
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-05-21 00:40:02 +0300

    homework 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="index"),
-    path("contacts/", contacts, name="contacts"),
+    path("catalog/<int:pk>/", product_detail, name="product_detail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, "home.html")
+    data = Product.objects.all()
+    context = {'product': data}
+    return render(request, "base_list.html", context)
 
 
-def contacts(request):
-    return render(request, "contacts.html")
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, "product_detail.html", context)
