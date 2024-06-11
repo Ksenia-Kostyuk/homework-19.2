61388c19a172a58aace031ebf5df1003acbfa610
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-06-11 15:59:21 +0300

    productCreate

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDitailView, ContactTemplateView
+from catalog.views import ProductListView, ProductDitailView, ContactTemplateView, ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="index"),
-    path("contact/", ContactTemplateView.as_view(), name="contacts"),
+    path("contact/", ContactTemplateView.as_view(), name="contact"),
     path("catalog/<int:pk>/", ProductDitailView.as_view(), name="product_detail"),
+    path("catalog/create/", ProductCreateView.as_view(), name="product_create"),
+    path("catalog/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from articles.engine import Article
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
 
 from catalog.models import Product
 
@@ -12,10 +13,22 @@ class ProductDitailView(DetailView):
     model = Product
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'category', 'price')
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'category', 'price')
+    success_url = reverse_lazy('product:product_list')
+
+
 class ContactTemplateView(TemplateView):
     template_name = 'catalog/contacts.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["latest_articles"] = Article.objects.all()
+        #context["latest_articles"] = Article.objects.all()
         return context
